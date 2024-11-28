69fa8a4ced071fab226d36007d23e14a150215be
kevinalej21 <kevi.kc7@gmail.com>
2024-11-28 08:02:25 -0500

    se centro la foto y se agrego estilo

--- kenp.php
+++ kenp.php
@@ -77,24 +77,27 @@
     <main>
         <div class="container">
             <section id="inicio">
-                <h2>Bienvenido a KENP</h2>
+                <h2 style>Bienvenido a KENP</h2>
                 <p>En KENP  podrás encontrar información sobre las bases tecnológicas que soportan el desarrollo
                     y ejecución de aplicaciones y servicios, como sistemas operativos, arquitecturas de almacenamiento,
                     y temas avanzados relacionados con la gestión de recursos y optimización de procesos.
-                </p>   
-                    <img src="imagenes/introduccion-sistemas-operativos.jpg" alt="SO">
+                </p>  
+                   <div style="text-align: center;">
+                      <img src="imagenes/introduccion-sistemas-operativos.jpg" alt="SO" style="box-shadow: 0 0 10px 10px rgba(128, 128, 128, 0.8); border-radius: 15px; max-width: 60%; height: auto;">
+                   </div> 
+                    
                 
 
             </section>
             <section id="contacto">
-                <h2>Contacto</h2>
+                <h2>Quienes somos</h2>
                 <p>Póngase en contacto con nosotros para más información.</p>
             </section>
         </div>
     </main>
 
     <footer>
-        <div class="container">
+        <div class="container.footer">
             <p>&copy; <?php echo date("Y"); ?> Mi Sitio Web. Todos los derechos reservados.</p>
             <p>Contacto: <a href="mailto:info@misitio.com">kevi.kc7@gmail.com</a> | Teléfono: (315) 633-5331</p>
             <p>Síguenos en: 
