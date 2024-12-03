eb620aaf36f397ff4363c4ab32518b5205037026
kevinalej21 <kevi.kc7@gmail.com>
2024-12-03 11:03:51 -0500

    se agrego video

--- kenp.php
+++ kenp.php
@@ -104,7 +104,7 @@
                 text-aling="center"
                 width="560" 
                 height="315" 
-                src="https://www.youtube.com/embed/LNGlxvBfAZo"
+                src="https://www.youtube.com/embed/Mw-Xttxj1RI"
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
